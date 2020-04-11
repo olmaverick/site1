960117ea3b8888a27698814a447fe00fa0b266dc
olmaverick <62926295+olmaverick@users.noreply.github.com>
2020-04-11 17:16:23 +0300

    Create lk.php

---
+++ lk.php
@@ -0,0 +1,52 @@
+<html lang="ru"><head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+      body{
+        height:100%;
+        background: url(img/depositphotos_10.jpg) no-repeat;
+        background-position:center center;
+        background-attachment: fixed;
+        background-size: cover;
+      }
+      .col-sm-5{
+        margin:0%;
+        box-shadow: 0 0 5px rgb(255,255,255,.9);
+        padding:1rem;
+        background: rgb(255,255,255,.2);
+        border-radius:2px;
+      }
+      .form-group{
+
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+        <div class="col-sm-5">
+          <h1 class="text-center text-light mb-5">Личный кабинет</h1>
+          <form>
+            <div class="form-group">
+            <div class="row">
+            <div>
+            E-MAIL  :
+            </div>
+            <div id="email"></div>
+            </div>
+            <div>
+            Имя     :
+            </div>
+            <div>
+            Фамилия :
+            </div>
+           </div>
+          </form>
+        </div>
+      </div>
+
+    <script>
+
+       
+    </script>
+  
+</body></html>
