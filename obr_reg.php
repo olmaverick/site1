68ac4ddc58fadb86675fffc1d7f10ca8c76feb7a
olmaverick <62926295+olmaverick@users.noreply.github.com>
2020-04-11 17:13:18 +0300

    Update obr_reg.php

--- obr_reg.php
+++ obr_reg.php
@@ -13,17 +13,19 @@
   $lastname  = $_POST['lastname'];
   $pass      = $_POST['pass'    ];
   
-  if (empty($email)or empty($name) or empty($lastname) or empty($pass)) exit('Не все поля заполнены');
+  if (empty($email)or empty($name) or empty($lastname) or empty($pass)) exit('0');
   
   $email     = mb_strtolower($email);//переводим строку емейла в нижний регистр
   $pass      = password_hash($pass,PASSWORD_BCRYPT,['cost'=> 12]);
   
-  $result   = $mysqli->query("SELECT `email ` FROM `users` WHERE `email `='$email'");
+  $result   = $mysqli->query("SELECT `email` FROM `users` WHERE `email`='$email'");//форма кавычек важна!!! 
+  //не вставлять лишние пробелы!!!
   //var_dump($result);
   
-  if (!empty($result->num_rows)) exit('Такой пользователь уже есть');
+  if (!empty($result->num_rows)) exit('1');
   
-  $mysqli->query("INSERT INTO `users`(`name`,`Lastname`,`email`,`pass`) VALUES ('$name','$lastname','$email','$pass')");
+  $mysqli->query("INSERT INTO `users`(`name`,`lastname`,`email`,`pass`) VALUES ('$name','$lastname','$email','$pass')");
+  echo '2';
  
   $text="Вы зарегистрировались на сайте ПНХ.РУ";
   $result=mail($email, 
@@ -31,4 +33,5 @@
    "Имя: $name, E-mail: $email,
     Сообщение:
       $text");
+     
   ?>
