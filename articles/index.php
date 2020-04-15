72e9bd203d1e98a5631e169a9c86b5df69e2e052
olmaverick <62926295+olmaverick@users.noreply.github.com>
2020-04-15 23:49:21 +0300

    Create index.php

---
+++ articles/index.php
@@ -0,0 +1,27 @@
+<?php
+include('../php/header.php');
+?>
+    <div class="container my-5">
+      <h1 class="text-center">Статьи</h1>
+      <div class="row my-5">
+        <div class="col-sm-4">
+          <h3>Lorem ipsum dolor sit amet. </h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur?</p>            
+          <a href="article1.php">read more>>></a>
+        </div>
+        <div class="col-sm-4">
+          <h3>Lorem ipsum dolor sit amet. </h3>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur?</p>            
+          <a href="article2.php">read more>>></a>
+        </div>
+        <div class="col-sm-4">
+          <h3>Lorem ipsum dolor sit amet. </h3>
+          <p> Lorem ipsum dolor sit.</p>            
+          <a href="article3.php">read more>>></a>
+        </div>
+      </div>
+    </div>
+  
+  <?php
+  include('../php/footer.php');
+  ?>
