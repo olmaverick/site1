cafaaa88851295434d000a0d3de5b6d3e78da02c
olmaverick <62926295+olmaverick@users.noreply.github.com>
2020-04-15 23:47:59 +0300

    Create article1.php

---
+++ articles/article1.php
@@ -0,0 +1,12 @@
+<?php
+include('../php/header.php');
+?>
+    <div class="container my-5">
+      <h1 class="text-center">Article 1</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, voluptatem, consequuntur!</p>
+    </div>
+  <?php
+  include('../php/footer.php');
+  ?>
